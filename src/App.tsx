e12a54396d4e0fbd10d9d822d1d5ab495032ae3d
Abdul Majid <abdulmajidcse@gmail.com>
2022-12-22 10:32:33 +0600

    menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import About from "./pages/About";
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
@@ -7,6 +7,10 @@ export default function App() {
   return (
     <>
       <div className="min-h-screen mx-auto dark:bg-slate-900/80 dark:text-white">
+        <div className="container mx-auto pt-5">
+          <Link to="/" className="p-1 text-lg text-blue-900 hover:text-white hover:bg-slate-900 bg-slate-400 hover:ring rounded mr-2">Home</Link>
+          <Link to="/about" className="p-1 text-lg text-blue-900 hover:text-white hover:bg-slate-900 bg-slate-400 hover:ring rounded">About</Link>
+        </div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="about" element={<About />} />
